ac6c61510ac692665a076a9625be2e60b1860041
Catalin Iordache <catalin.iordache@net.ase.ro>
2021-11-13 22:17:28 +0200

    First commit

---
+++ s07_e04/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const express = require("express");
+const { departments } = require("./db");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/departments", (req, res) => {
+  res.status(200).json(departments);
+});
+
+app.get("/departments/:id", (req, res) => {
+  const department = departments.find(
+    (department) => department.id === Number(req.params.id)
+  );
+  if (department) {
+    res.status(200).json(department);
+  } else {
+    res.status(404).json({ error: "Entity not found" });
+  }
+});
+
+app.set("port", process.env.PORT || 7000);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server started on http://localhost:${app.get("port")}`);
+});
